d65aac07f071963be52783f8b720e0f8e4defd90
Shyaboi <ian.shane.sears@gmail.com>
2020-09-11 23:12:30 -0700

    added readme and random refream in a range

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ http.get('http://glacial-beyond-53964.herokuapp.com/', (res)=> {res.pipe(cs)})
 
 cs.on('total',(count)=> {console.log("total matches: " + count)})
 
-console.log('__dirname:', __dirname);
-console.log('__filename:', __filename);
+// console.log('__dirname:', __dirname);
+// console.log('__filename:', __filename);
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import os
 import time
-import datetime
+import random
 from datetime import date
+
 timeStamp = date.today()
 
 hour = time.strftime("%H")
@@ -12,8 +13,10 @@ f= open("muhfile.txt","a+")
 print(nowTime)
 
 def cronLoop():
+    rando = random.randrange(244, 456)
+    print(rando)
     f.write(f"\nLast cronned at: {timeStamp} | {nowTime}\n")
     os.system('node index.js /F');print("ran node cronish")
-    time.sleep(300)
+    time.sleep(rando)
     cronLoop()
 cronLoop()
