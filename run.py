61c071b8c89176990c2d4c25a6abce9f06e6ca14
Shyaboi <ian.shane.sears@gmail.com>
2020-09-11 22:13:54 -0700

    made ghetto log file

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 import os
 import time
+import datetime
+from datetime import date
+timeStamp = date.today()
+
+hour = time.strftime("%H")
+minn = time.strftime("%M")
+sec = time.strftime("%S")
+nowTime = f"{hour}:{minn};{sec}"
+f= open("muhfile.txt","a+")
+print(nowTime)
 
 def cronLoop():
+    f.write(f"\nLast cronned at: {timeStamp} | {nowTime}\n")
+    f.close() 
     os.system('node index.js /F');print("ran node cronish")
     time.sleep(300)
     cronLoop()
